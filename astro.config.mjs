e23585e78d463f306296de40444f3802c1c34558
Alejo Arenas Moreira <arenasmoreira@gmail.com>
2025-12-30 18:19:25 +0100

    Fix on images loading performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Le decimos a Astro que pase esta config a Vite
+  vite: {
+    server: {
+      hmr: false, // Ahora sí: Desactiva la recarga automática
+    },
+  },
+});
